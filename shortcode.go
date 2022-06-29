95b4482581e13630399859265fa0e60fa42638b6
Milan Lesichkov <lesichkovm@gmail.com>
2022-06-29 07:34:34 +0000

    Added support for multiple bracket types

--- shortcode.go
+++ shortcode.go
@@ -1,64 +1,155 @@
 package shortcode
 
 import (
+	"errors"
 	"log"
 	"regexp"
 	"strings"
 )
 
-type Args map[string]string
+// Shortcode defines a shortcode engine
+type Shortcode struct {
+	bracketOpening string
+	bracketClosing string
+}
 
-func parse(str string, shortcode string, fn func(map[string]string) string) string {
-	regex := regexp.MustCompile(`<` + shortcode + `(.*)>([\S\s]+)</` + shortcode + `>`)
-	//matches := re.FindAllStringSubmatch(str, -1)
-	//log.Println(matches)
-	log.Println("=============")
-	// log.Println(content)
+// ShortcodeOption options for the shortcode
+type ShortcodeOption func(*Shortcode)
 
-	for {
-		match := regex.FindStringSubmatchIndex(str)
-		if match == nil {
-			break
-		}
-		args := Args{}
-		openingTagStart, openingTagClose, tagNameStart, tagNameEnd := match[0], match[1], match[2], match[3]
-		//fullMatch := str[openingTagStart:openingTagClose]
-		tagName := str[tagNameStart:tagNameEnd]
-		log.Println("TAG NAME: " + tagName)
+// WithBracketType sets the type of bracket used for the shortcode
+func WithBrackets(bracketOpening string, bracketClosing string) ShortcodeOption {
+	return func(s *Shortcode) {
+		s.bracketOpening = bracketOpening
+		s.bracketClosing = bracketClosing
+	}
+}
 
-		// Parse the arguments
-		if match[4] != -1 {
-			argsString := str[match[4]:match[5]]
-			argsRegex := regexp.MustCompile(`\s*([^=]+)="([^"]+)"`)
-			for _, argMatch := range argsRegex.FindAllStringSubmatch(argsString, -1) {
-				args[argMatch[1]] = argMatch[2]
-			}
-		}
-
-		//var closingTagEnd = openingTagClose
-		var textToReplace = str[openingTagStart:openingTagClose]
-
-		//replaced := s.registered[tagName](args)
-		replaced := fn(args)
-
-		str = strings.Replace(str, textToReplace, replaced, 1)
+// NewShortcode creates a new shortcode engine
+func NewShortcode(opts ...ShortcodeOption) (*Shortcode, error) {
+	shortcode := &Shortcode{}
+	for _, opt := range opts {
+		opt(shortcode)
 	}
 
-	// for _, match := range matches {
-	// 	log.Println("==== MATCH ====")
-	// 	if match[0] == "" {
-	// 		continue
-	// 	}
-	// 	//log.Println(match[0])
-	// 	// attrs := match[1]
-	// 	// text := (match[2])
-	// 	match := regex.FindStringSubmatchIndex(text)
-	// 	if match == nil {
-	// 		break
-	// 	}
+	if shortcode.bracketOpening == "" {
+		return nil, errors.New("shortcode: bracketOpening is required")
+	}
 
-	// 	str = strings.Replace(str, textToReplace, replaced, 1)
-	// }
+	if shortcode.bracketClosing == "" {
+		return nil, errors.New("shortcode: bracketClosing is required")
+	}
+
+	return shortcode, nil
+}
+
+func (sh Shortcode) Render(str string, shortcode string, fn func(map[string]string) string) string {
+	escapedBracketOpening := strings.ReplaceAll(sh.bracketOpening, "[", "\\[")
+	escapedBracketOpening = strings.ReplaceAll(escapedBracketOpening, "(", "\\(")
+	escapedBracketClosing := strings.ReplaceAll(sh.bracketClosing, "]", "\\]")
+	escapedBracketClosing = strings.ReplaceAll(escapedBracketClosing, ")", "\\)")
+	attr := `(\s+[^` + escapedBracketClosing + `]+)?`
+	start := escapedBracketOpening + shortcode + attr + escapedBracketClosing
+	end := escapedBracketOpening + `/` + shortcode + escapedBracketClosing
+	// content := `([\S\s]+)`
+	// content := `([^\[]*)`
+	content := `([^` + escapedBracketClosing + `]*)`
+
+	regex := regexp.MustCompile(start + content + end)
+	for _, match := range regex.FindAllStringSubmatch(str, -1) {
+		if match[0] == "" {
+			continue
+		}
+		attrs, content := match[1], match[2]
+		log.Println(attrs, content)
+		shortcodeResult := fn(attrsToArgs(attrs))
+		str = strings.Replace(str, match[0], shortcodeResult, 1)
+	}
 
 	return str
 }
+
+func attrsToArgs(attrs string) map[string]string {
+	args := map[string]string{}
+	argsRegex := regexp.MustCompile(`\s*([^=]+)="([^"]+)"`)
+	for _, argMatch := range argsRegex.FindAllStringSubmatch(attrs, -1) {
+		args[argMatch[1]] = argMatch[2]
+	}
+	return args
+}
+
+// type Args map[string]string
+
+// func (sh Shortcode) Render(str string, shortcode string, fn func(map[string]string) string) string {
+// 	escapedBracketOpening := strings.ReplaceAll(sh.bracketOpening, "[", "\\[")
+// 	escapedBracketClosing := strings.ReplaceAll(sh.bracketClosing, "]", "\\]")
+// 	start := escapedBracketOpening + shortcode + `(.*)` + escapedBracketClosing
+// 	//end := escapedBracketOpening + `/` + shortcode + escapedBracketClosing
+// 	content := `([\S\s]+)`
+// 	regex := regexp.MustCompile(start + content)
+// 	//matches := re.FindAllStringSubmatch(str, -1)
+// 	//log.Println(matches)
+// 	log.Println("=============")
+// 	// log.Println(content)
+
+// 	for {
+// 		match := regex.FindStringSubmatchIndex(str)
+// 		if match == nil {
+// 			break
+// 		}
+// 		// args := Args{}
+// 		log.Println("MATCH 0:")
+// 		log.Println(match[0])
+
+// 		log.Println("MATCH 1:")
+// 		log.Println(match[1])
+
+// 		log.Println("MATCH 2:")
+// 		log.Println(match[2])
+
+// 		log.Println("MATCH 3:")
+// 		log.Println(match[3])
+
+// 		log.Println("MATCH 4:")
+// 		log.Println(match[4])
+
+// 		//openingTagStart, openingTagClose, tagNameStart, tagNameEnd := match[0], match[1], match[2], match[3]
+// 		startStr, endStr, tagNameStart, tagNameEnd := match[0], match[1], match[2], match[3]
+// 		fullMatch := str[startStr:endStr]
+// 		tagName := str[tagNameStart:tagNameEnd]
+// 		log.Println("FULL MATCH: " + fullMatch)
+// 		log.Println("TAG NAME: " + tagName)
+
+// 		// Parse the arguments
+// 		// if match[4] != -1 {
+// 		// 	argsString := str[match[4]:match[5]]
+// 		// 	argsRegex := regexp.MustCompile(`\s*([^=]+)="([^"]+)"`)
+// 		// 	for _, argMatch := range argsRegex.FindAllStringSubmatch(argsString, -1) {
+// 		// 		args[argMatch[1]] = argMatch[2]
+// 		// 	}
+// 		// }
+// 		// var closingTagEnd = openingTagClose
+// 		// var textToReplace = str[tagNameStart:tagNameEnd]
+
+// 		// replaced := fn(args)
+
+// 		// str = strings.Replace(str, textToReplace, replaced, 1)
+// 	}
+
+// 	// for _, match := range matches {
+// 	// 	log.Println("==== MATCH ====")
+// 	// 	if match[0] == "" {
+// 	// 		continue
+// 	// 	}
+// 	// 	//log.Println(match[0])
+// 	// 	// attrs := match[1]
+// 	// 	// text := (match[2])
+// 	// 	match := regex.FindStringSubmatchIndex(text)
+// 	// 	if match == nil {
+// 	// 		break
+// 	// 	}
+
+// 	// 	str = strings.Replace(str, textToReplace, replaced, 1)
+// 	// }
+
+// 	return str
+// }
--- shortcode_test.go
+++ shortcode_test.go
@@ -1,26 +1,140 @@
 package shortcode
 
 import (
-	"log"
 	"testing"
 )
 
-func TestTrue(t *testing.T) {
-	text := `<x-text id="222"> 
-	TEST    
-	</x-text>`
+func TestReturnsErrorOnMisconfiguration(t *testing.T) {
+	_, err := NewShortcode()
 
-	parsed := parse(text, "x-text", testShortcode)
+	if err == nil {
+		t.Fatalf("Shortcode must throw one error")
+	}
 
-	isOk := parsed == "SHORTCODE"
+	expectedError := "shortcode: bracketOpening is required"
+	if err.Error() != expectedError {
+		t.Fatalf("Shortcode must return error of '%s', instead returned '%s'", expectedError, err.Error())
+	}
 
-	if isOk == false {
-		log.Println(parsed)
-		t.Fatalf("Shortcode could not be found")
+	_, err2 := NewShortcode(WithBrackets("", ""))
+
+	if err2 == nil {
+		t.Fatalf("Shortcode must throw one error")
+	}
+
+	expectedError2 := "shortcode: bracketOpening is required"
+	if err2.Error() != expectedError2 {
+		t.Fatalf("Shortcode must return error of '%s', instead returned '%s'", expectedError2, err2.Error())
+	}
+
+	_, err3 := NewShortcode(WithBrackets("[", ""))
+
+	if err3 == nil {
+		t.Fatalf("Shortcode must throw one error")
+	}
+
+	expectedError3 := "shortcode: bracketClosing is required"
+	if err3.Error() != expectedError3 {
+		t.Fatalf("Shortcode must return error of '%s', instead returned '%s'", expectedError3, err3.Error())
+	}
+}
+
+func TestWithSquareBrackets(t *testing.T) {
+	sh, err := NewShortcode(WithBrackets("[", "]"))
+
+	if err != nil {
+		t.Fatalf("Shortcode must not throw an error, but '%s'", err.Error())
+	}
+
+	text := `TEXT BEFORE [x-text id="222"]
+	TEST
+	[/x-text] TEXT AFTER`
+
+	parsed := sh.Render(text, "x-text", testShortcode)
+
+	expected := "TEXT BEFORE SHORTCODE WITH ID 222 TEXT AFTER"
+
+	if parsed != expected {
+		t.Fatalf("Expected '%s', instead returned '%s'", expected, parsed)
+	}
+}
+
+func TestWithBrackets(t *testing.T) {
+	sh, err := NewShortcode(WithBrackets("(", ")"))
+
+	if err != nil {
+		t.Fatalf("Shortcode must not throw an error, but '%s'", err.Error())
+	}
+
+	text := `TEXT BEFORE (x-text id="222")
+	TEST
+	(/x-text) TEXT AFTER`
+
+	parsed := sh.Render(text, "x-text", testShortcode)
+
+	expected := "TEXT BEFORE SHORTCODE WITH ID 222 TEXT AFTER"
+
+	if parsed != expected {
+		t.Fatalf("Expected '%s', instead returned '%s'", expected, parsed)
+	}
+}
+
+func TestWithAngleBrackets(t *testing.T) {
+	sh, err := NewShortcode(WithBrackets("<", ">"))
+
+	if err != nil {
+		t.Fatalf("Shortcode must not throw an error, but '%s'", err.Error())
+	}
+
+	text := `TEXT BEFORE <x-text id="222">
+	TEST
+	</x-text> TEXT AFTER`
+
+	parsed := sh.Render(text, "x-text", testShortcode)
+
+	expected := "TEXT BEFORE SHORTCODE WITH ID 222 TEXT AFTER"
+
+	if parsed != expected {
+		t.Fatalf("Expected '%s', instead returned '%s'", expected, parsed)
+	}
+}
+
+func TestWithMultipleSquareBrackets(t *testing.T) {
+	sh, err := NewShortcode(WithBrackets("[", "]"))
+
+	if err != nil {
+		t.Fatalf("Shortcode must not throw an error, but '%s'", err.Error())
+	}
+
+	text := `TEXT BEFORE [x-text id="111"]TEST[/x-text] [x-text id="222"]TEST[/x-text] TEXT AFTER`
+
+	parsed := sh.Render(text, "x-text", testShortcode)
+
+	expected := "TEXT BEFORE SHORTCODE WITH ID 111 SHORTCODE WITH ID 222 TEXT AFTER"
+
+	if parsed != expected {
+		t.Fatalf("Expected '%s', instead returned '%s'", expected, parsed)
+	}
+}
+
+func TestWithMultipleAngleBrackets(t *testing.T) {
+	sh, err := NewShortcode(WithBrackets("<", ">"))
+
+	if err != nil {
+		t.Fatalf("Shortcode must not throw an error, but '%s'", err.Error())
+	}
+
+	text := `TEXT BEFORE <x-text id="111">TEST</x-text> <x-text id="222">TEST</x-text> TEXT AFTER`
+
+	parsed := sh.Render(text, "x-text", testShortcode)
+
+	expected := "TEXT BEFORE SHORTCODE WITH ID 111 SHORTCODE WITH ID 222 TEXT AFTER"
+
+	if parsed != expected {
+		t.Fatalf("Expected '%s', instead returned '%s'", expected, parsed)
 	}
 }
 
 func testShortcode(args map[string]string) string {
-	log.Println(args)
-	return "SHORTCODE"
+	return "SHORTCODE WITH ID " + args["id"]
 }
